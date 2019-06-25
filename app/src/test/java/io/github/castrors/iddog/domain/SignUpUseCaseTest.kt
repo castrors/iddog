a20716495af103b9b32b741731ce934010f0f4f0
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2019-06-25 14:02:17 -0300

    Add tests to signUpUseCase

---
+++ app/src/test/java/io/github/castrors/iddog/domain/SignUpUseCaseTest.kt
@@ -0,0 +1,56 @@
+package io.github.castrors.iddog.domain
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.github.castrors.iddog.data.DogsGateway
+import io.github.castrors.iddog.data.base.Either
+import io.github.castrors.iddog.data.model.UserEntity
+import io.github.castrors.iddog.presentation.base.ContentState
+import io.github.castrors.iddog.presentation.base.ErrorState
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class SignUpUseCaseTest {
+    private lateinit var signUpUseCase: SignUpUseCase
+    private val dogsGateway: DogsGateway = mock()
+
+    @Before
+    fun setup() {
+        signUpUseCase = SignUpUseCase(dogsGateway)
+    }
+
+    @Test
+    fun givenGetDogs_whenInvokeGatewayWithSuccess_thenShouldMapDogsList() {
+        runBlocking {
+            val email = "expected@email.com"
+            val expectedResult = UserEntity("expected_id", "expected@email.com", "expected_token")
+            whenever(dogsGateway.signUp(email)).thenReturn(
+                Either.Right(
+                    expectedResult
+                )
+            )
+
+            val uiState = signUpUseCase.run(email)
+            val contentState = uiState as ContentState
+
+            assertEquals(contentState.content, expectedResult)
+        }
+    }
+
+    @Test
+    fun givenGetDogs_whenInvokeGatewayWithFailure_thenShouldReturnErrorState() {
+        runBlocking {
+            val email = "expected@email.com"
+            val expectedResult = Exception("Unmapped exception")
+            whenever(dogsGateway.signUp(email)).thenReturn(Either.Left(expectedResult))
+
+            val uiState = signUpUseCase.run(email)
+            val errorState = uiState as ErrorState
+
+            assertEquals(errorState.error, expectedResult)
+        }
+
+    }
+}
