798fb7a0b0a88ed9a4cfa0b2d30f43ca1c1e48b6
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2019-06-25 14:01:58 -0300

    Implement signup use case

---
+++ app/src/main/java/io/github/castrors/iddog/domain/SignUpUseCase.kt
@@ -0,0 +1,23 @@
+package io.github.castrors.iddog.domain
+
+import io.github.castrors.iddog.data.DogsGateway
+import io.github.castrors.iddog.domain.base.SuspendedInteractor
+import io.github.castrors.iddog.presentation.base.ContentState
+import io.github.castrors.iddog.presentation.base.ErrorState
+import io.github.castrors.iddog.presentation.base.UIState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
+
+class SignUpUseCase(
+    private val dogsGateway: DogsGateway
+) : SuspendedInteractor<String, UIState> {
+    override suspend fun run(params: String): UIState {
+        return withContext(IO) {
+            val result = dogsGateway.signUp(params)
+            result.either(
+                { error -> ErrorState(error) },
+                { userEntity -> ContentState(userEntity) }
+            ) as UIState
+        }
+    }
+}
