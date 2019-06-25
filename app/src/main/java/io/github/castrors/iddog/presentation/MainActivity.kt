4b161fa2e8fb8e05a62e4218690b59834da7cfc8
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2019-06-24 23:33:26 -0300

    First commit

---
+++ app/src/main/java/io/github/castrors/iddog/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.castrors.iddog.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.castrors.iddog.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
